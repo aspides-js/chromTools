dd329e6f92b7266d3f97df8410d7c430e18a3568
Jessica Shields <jess.m.shields@live.co.uk>
2023-02-28 10:01:52 +0000

    fix genome size and chr lens specification

--- entirety/constants.py
+++ entirety/constants.py
@@ -1,16 +1,16 @@
 COMPLETE_VERSION = "1.0"
-GENOME = {"ce10":"ce10.txt",
-			"ce11":"ce11.txt",
-			"ce6":"ce6.txt",
-			"danRer10":"danRer10.txt",
-			"danRer11":"danRer11.txt",
-			"danRer7":"danRer7.txt",
-			"dm3":"dm3.txt",
-			"dm6":"dm6.txt",
-			"hg18":"hg18.txt",
-			"hg19":"hg19.txt",
-			"hg38":"tmp.txt",
-			"mm10":"mm10.txt",
-			"mm9":"mm9.txt",
-			"rn5":"rn5.txt",
-			"rn6":"rn6.txt"}
+GENOME = {"ce10":["ce10.txt", 100286401],
+			"ce11":["ce11.txt", 100286401],
+			"ce6":["ce6.txt", 100286401],
+			"dm3":["dm3.txt", 142573017],
+			"dm6":["dm6.txt", 142573017],
+			"hg18":["hg18.txt", 2913022398],
+			"hg19":["hg19.txt", 2913022398],
+			"hg38":["hg38.txt", 2913022398],
+			"mm10":["mm10.txt", 2652783500],
+			"mm9":["mm9.txt", 2652783500]}
+
+EFFECTIVEGS = {"hs":2913022398,                  #GRCh38
+               "mm":2652783500,                  #GRCm38
+               "ce":100286401,                   #WBcel235
+               "dm":142573017}                   #dm6
