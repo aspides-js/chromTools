d751c6264b0721fdfb1e89b402747d275d337f40
Jessica Shields <jess.m.shields@live.co.uk>
2023-07-19 11:38:47 +0100

    cython build

---
+++ c_build.py
@@ -0,0 +1,20 @@
+from setuptools import Extension
+from setuptools.command.build_py import build_py as _build_py
+
+
+class build_py(_build_py):
+    def run(self):
+        self.run_command("build_ext")
+        return super().run()
+
+    def initialize_options(self):
+        super().initialize_options()
+        if self.distribution.ext_modules == None:
+            self.distribution.ext_modules = []
+
+        self.distribution.ext_modules.append(
+            Extension(
+                "chromTools.c_io",
+                sources=["chromTools/c_io.pyx"],
+            )
+        )
