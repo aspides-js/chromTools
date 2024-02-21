e0670421f2827aa562229c6d830a09010c0f3887
Jessica Shields <shieldj@login002.nemo.thecrick.org>
2024-02-21 11:19:48 +0000

    update docstrings

--- chromTools/c_io.pyx
+++ chromTools/c_io.pyx
@@ -15,7 +15,7 @@ import mmh3
 import sys
 cimport numpy as np
 
-from chromTools.mmh cimport murmurhash
+from chromTools.mmh cimport murmurhash_32
 
 #import chromTools.complete_cmd
 
@@ -23,12 +23,28 @@ from chromTools.mmh cimport murmurhash
 # Misc function(s)
 # ------------------------------------
 
-cdef int c_strand_check(bytes szstrand, int szbeginline, int szendline):
+cdef int c_strand_check(bytes szstrand, int szbeginline, int szendline, int noffsetleft, int noffsetright, int nbinsize, int nshift):
+    """
+    Determine the bin index based on strand information.
+
+    This function calculates the bin index for a given genomic region, taking into account the strand information.
+
+    :param bytes szstrand: The strand information as bytes, either b"+\\n" for positive strand or b"-\\n" for negative strand.
+    :param int szbeginline: The starting position of the genomic region.
+    :param int szendline: The ending position of the genomic region.
+    :param int noffsetleft: Offset for the left coordinate so it is 0-based inclusive
+    :param int noffsetright: Offset for the right coordinate based on bed files being 0-based but not inclusive
+    :param int nbinsize: The size of the bins. default 200
+    :param int nshift: Shift value for calculating the bin index.
+
+    :return: The calculated bin index based on the provided parameters.
+    :rtype: int
+    """
     cdef int nbin 
     if szstrand == b"+\n":
-        nbin = (szbeginline - 0 + 100) // 200
+        nbin = (szbeginline - noffsetleft + nshift) // nbinsize
     elif szstrand == b"-\n":
-        nbin = (szendline - 1 - 100) // 200
+        nbin = (szendline - noffsetright - nshift) // nbinsize
     return nbin
 
 
@@ -49,8 +65,8 @@ cpdef read_to_grid(file_path,
     Reads data from a file and populates a grid with the data.
 
     This function reads the specified file line by line, processes each line, and adds the data to the grid.
-    The grid is a three-dimensional array that represents the data in a structured manner based on the provided
-    parameters.
+    The grid is a three-dimensional array that represents each chromosome, the lengths of each chr divided by binsize, 
+    and the number of marks (always 1 in chromTools_complete).
 
     :param str file_path: The path to the file to be read.
     :param dict hmchrom: A dictionary containing mappings of chromosome names to chromosome indices.
@@ -100,7 +116,8 @@ cpdef read_to_grid(file_path,
             szendline = atoi(szLine[nendcol])
 
             # run check for positive/negative strand to output nbin
-            nbin = c_strand_check(szstrand, szbeginline, szendline)
+            nbin = c_strand_check(szstrand, szbeginline, szendline, noffsetleft, noffsetright, nbinsize, nshift)
+
             if nbin >= 0 and nbin < shape:
                 grid[nchrom, nbin, nmark] += 1
                 bpresent[nchrom] = 1
@@ -114,15 +131,21 @@ cpdef read_to_grid(file_path,
 # --------------------------------------------------------------------------------#                         
 
 cpdef int c_subsample(str file_path, str outf_path, int a, int seed):
-    """Generate a random hash from readname using seed. If number above proportional cut-off (True), discard read.
+    """
+    Subsample reads from a file based on a hash threshold.
 
-    Args:
-            maxHashValue (int): Threshold above which reads are discarded
-            seed (int): Random seed
-            line (str): Read/line in file
+    This function reads a file line by line, extracts the readname, hashes it using the MurmurHash3_32 algorithm, 
+    and writes the original line to a new file if the hash value is below a specified threshold, calculated 
+    based on the proportion of lines to be retained. If number above proportional cut-off (True), discard read.
 
-    Returns:
-            bool: Boolean specifying if readname is below or above discard threshold
+
+    :param str file_path: Path to the input file.
+    :param str outf_path: Path to the output file.
+    :param int a: Hash threshold; reads with hash values above this threshold will be skipped.
+    :param int seed: Seed for the MurmurHash3_32 algorithm.
+
+    :return: The number of reads written to the output file.
+    :rtype: int
     """
     # Open the file
     cdef FILE *file = fopen(file_path.encode(), "r")
@@ -144,7 +167,7 @@ cpdef int c_subsample(str file_path, str outf_path, int a, int seed):
         c_readname_len = strlen(c_readname)
 
         # use c function murmurhash to hash string
-        hashInt = murmurhash(c_readname, c_readname_len, seed)
+        hashInt = murmurhash_32(c_readname, c_readname_len, seed)
         
         # if int is above threshold then skip, otherwise write
         if hashInt > a: 
